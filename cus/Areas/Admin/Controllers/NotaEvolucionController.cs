664c14746cec6ddd96624a38123b4b865481598e
damianheros <sistemas.girocreativo@gmail.com>
2023-10-03 14:12:04 -0600

    controlador de nota de evolucion y cambios en los checkbox de la historia clinica

---
+++ cus/Areas/Admin/Controllers/NotaEvolucionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CUS.Areas.Admin.Controllers
+{
+    public class NotaEvolucionController : Controller
+    {
+        // GET: Admin/NotaEvolucion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
