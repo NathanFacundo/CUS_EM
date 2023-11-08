20d1683802312bb78cd1e5de0d46644301c7d63a
damianheros <sistemas.girocreativo@gmail.com>
2023-11-08 16:39:51 -0600

    vista de enfermeria y cambio al crear un nuevo paciente

---
+++ cus/Areas/Admin/Controllers/EnfermeriaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CUS.Areas.Admin.Controllers
+{
+    public class EnfermeriaController : Controller
+    {
+
+        Models.CUS db = new Models.CUS();
+
+        // GET: Admin/Enfermeria
+        public ActionResult Index(string expediente)
+        {
+            if (expediente != null)
+            {
+                var paciente = (from a in db.Paciente
+                                where a.Expediente == expediente
+                                select a).FirstOrDefault();
+
+                return View(paciente);
+            }
+            else
+            {
+                return RedirectToAction("BuscarPaciente", "DerechoHabiente");
+            }
+
+        }
+    }
+}
