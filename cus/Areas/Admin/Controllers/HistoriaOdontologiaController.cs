83708c46638de522e88e7b3b9f8281897792025b
Milton Facundo <miltonfacu96@gmail.com>
2023-11-08 17:13:00 -0600

    se agrega nueva hc. Odontologia

---
+++ cus/Areas/Admin/Controllers/HistoriaOdontologiaController.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace CUS.Areas.Admin.Controllers
+{
+    public class HistoriaOdontologiaController : Controller
+    {
+        Models.CUS db = new Models.CUS();
+        Models.HC_Nutricion hcNut = new Models.HC_Nutricion();
+
+        // GET: Admin/HistoriaOdontologia
+        public ActionResult Index(string expediente)
+        {
+            if (expediente != null)
+            {
+                var paciente = (from a in db.Paciente
+                                where a.Expediente == expediente
+                                select a).FirstOrDefault();
+
+                return View(paciente);
+            }
+            else
+            {
+                return RedirectToAction("BuscarPaciente", "DerechoHabiente");
+            }
+        }
+
+        
+    }
+}
