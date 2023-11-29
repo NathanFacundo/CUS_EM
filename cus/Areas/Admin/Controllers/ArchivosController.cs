8e258bf73d962c0c62bebd6892e27453d0dcc7c2
damianheros <sistemas.girocreativo@gmail.com>
2023-11-28 21:08:46 -0600

    archivos y recetas

---
+++ cus/Areas/Admin/Controllers/ArchivosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CUS.Areas.Admin.Controllers
+{
+    public class ArchivosController : Controller
+    {
+        Models.CUS db = new Models.CUS();
+        // GET: Admin/Archivos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create(string expediente)
+        {
+            if(expediente != null)
+            {
+                var paciente = (from a in db.Paciente
+                                where a.Expediente == expediente
+                                select a).FirstOrDefault();
+
+                return View(paciente);
+            }
+            else
+            {
+                return RedirectToAction("BuscarPaciente", "DerechoHabiente");
+            }
+        }
+
+
+    }
+}
---
+++ cus/Areas/Admin/Controllers/RecetasController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CUS.Areas.Admin.Controllers
+{
+
+    public class RecetasController : Controller
+    {
+
+        Models.CUS db = new Models.CUS();
+
+
+        // GET: Admin/Recetas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Create(string expediente)
+        {
+            if (expediente != null)
+            {
+                var paciente = (from a in db.Paciente
+                                where a.Expediente == expediente
+                                select a).FirstOrDefault();
+
+                return View(paciente);
+            }
+            else
+            {
+                return RedirectToAction("BuscarPaciente", "DerechoHabiente");
+            }
+
+        }
+
+
+    }
+}
