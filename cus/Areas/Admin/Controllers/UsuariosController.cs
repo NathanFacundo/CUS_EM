f7235c37eee11973ce0266d5723b93292da19d9d
Milton Facundo <miltonfacu96@gmail.com>
2024-02-06 14:18:30 -0600

    guardar usuarios con sus roles

--- cus/Areas/Admin/Controllers/UsuariosController.cs
+++ cus/Areas/Admin/Controllers/UsuariosController.cs
@@ -14,7 +14,7 @@ namespace CUS.Areas.Admin.Controllers
     [Authorize]
     public class UsuariosController : Controller
     {
-
+        private Models.CUS db = new Models.CUS();
         private ApplicationUserManager _userManager;
 
 
@@ -55,10 +55,49 @@ namespace CUS.Areas.Admin.Controllers
             };
 
             var result = await UserManager.CreateAsync(user, model.Password);
-
             //Se le agrega el rol
             //user.Roles.Add(new IdentityUserRole { RoleId = role1 });
 
+
+            //Agregar el rol al usuario recién crado
+            //1-Recepcion. 2-Expediente. 3-Admin. 4-Enfermeria
+
+            if (model.AdminUsuarios) //Admin (todos los roles)
+            {
+                //hacer el update para insertar en AspNetUserRoles
+                string userId = user.Email;
+                string roleId = "3";
+
+                string sqlQuery = $"UPDATE AspNetUserRoles SET UserId = '{userId}', RoleId = '{roleId}'";
+                db.Database.ExecuteSqlCommand(sqlQuery);
+            }
+            else
+            {
+                if (model.AdminPacientes)//Recepción (registro de px)
+                {
+                    string userId = user.Email;
+                    string roleId = "1";
+
+                    string sqlQuery = $"UPDATE AspNetUserRoles SET UserId = '{userId}', RoleId = '{roleId}'";
+                    db.Database.ExecuteSqlCommand(sqlQuery);
+                }
+                else if (model.ExpMedico)//Expediente
+                {
+                    string userId = user.Email;
+                    string roleId = "2";
+
+                    string sqlQuery = $"UPDATE AspNetUserRoles SET UserId = '{userId}', RoleId = '{roleId}'";
+                    db.Database.ExecuteSqlCommand(sqlQuery);
+                }
+                else //Enfermeria
+                {
+                    string userId = user.Email;
+                    string roleId = "4";
+
+                    string sqlQuery = $"UPDATE AspNetUserRoles SET UserId = '{userId}', RoleId = '{roleId}'";
+                    db.Database.ExecuteSqlCommand(sqlQuery);
+                }
+            }
 
             return RedirectToAction("Index", "Usuarios");
             //return View();
